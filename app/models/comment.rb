de2d0a7517cc63c14e60ce3fb50f712c12c357c0
anthony maseda <ajmaseda@sixoverground.com>
2014-11-15 00:09:52 -0500

    max and min character lengths

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+  validates :commenter, presence: true,
+                    length: { minimum: 4, maximum: 12 }
   validates :body, presence: true,
-                    length: { minimum: 5 }
+                    length: { minimum: 4, maximum: 120 }
 end

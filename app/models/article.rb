8b39ea986d682a04b8304eef09694073a55a84dc
anthony maseda <ajmaseda@sixoverground.com>
2014-11-15 23:44:20 -0500

    character lengths

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
-                    length: { minimum: 2, maximum: 20 }
+                    length: { minimum: 2, maximum: 50 }
   validates :text, presence: true,
                     length: { minimum: 2, maximum: 400 }
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
   validates :commenter, presence: true,
-                    length: { minimum: 2, maximum: 20 }
+                    length: { minimum: 2, maximum: 50 }
   validates :body, presence: true,
                     length: { minimum: 2, maximum: 200 }
 end

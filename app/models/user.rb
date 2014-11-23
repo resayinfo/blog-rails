fc930ba7903f8ae58f8c93db1f3010207d725408
anthony maseda <ajmaseda@sixoverground.com>
2014-11-23 02:07:05 -0500

    def name as first and last

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
   validates :password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
 end

d882af2e1471c395662c00e8a4fd0f5e1ab5e9e6
anthony maseda <ajmaseda@sixoverground.com>
2014-11-25 00:17:43 -0500

    no youtube module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  # def youtube_video(url)
-  #   render :partial => 'shared/youtube', :locals => { :url => url }
-  # end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,2 @@
 module ArticlesHelper
-
-  # def embed(youtube_url)
-  #   youtube_id = youtube_url.split("=").last
-  #   content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
-  # end
-
 end

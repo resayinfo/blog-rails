52c308551a22bd958206487fc2b35f4b11e94e25
anthony maseda <ajmaseda@sixoverground.com>
2014-11-25 20:27:54 -0500

    minimum: 2, maximum: 40/400 char length

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,9 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :author, presence: true,
-                    length: { minimum: 2, maximum: 50 }
+                    length: { minimum: 2, maximum: 40 }
   validates :title, presence: true,
-                    length: { minimum: 2, maximum: 50 }
+                    length: { minimum: 2, maximum: 40 }
   validates :text, presence: true,
                     length: { minimum: 2, maximum: 400 }
 end

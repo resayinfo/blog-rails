914a66ba3d750809f4eb3cc6ba2aca9921d846b7
anthony maseda <ajmaseda@sixoverground.com>
2014-11-29 23:39:56 -0500

    uninitialized constant Article

---
+++ spec/views/articles/index.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "articles/index" do
+
+  context "with 2 articles" do
+    before(:each) do
+      assign(:articles, [
+        Article.create!(:author => "Apu", :title => "Sewing", :text => "Sewing is fun"),
+        Article.create!(:author => "Willy", :title => "Digging", :text => "Digging is fun")
+        ])
+      end
+
+      it "displays both articles" do
+        render
+
+        expect(rendered).to match /Sewing/
+        expect(rendered).to match /Digging/
+      end
+    end
+  end

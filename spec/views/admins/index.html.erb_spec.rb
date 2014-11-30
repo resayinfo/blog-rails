f6d61b8a79446b3c8c0259d63cad6bc7066ddb49
anthony maseda <ajmaseda@sixoverground.com>
2014-11-29 23:14:06 -0500

    view specs set up .. uninitialized constant User

---
+++ spec/views/admins/index.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "admins/index" do
+
+  context "with 2 admins" do
+    before(:each) do
+      assign(:admins, [
+        User.create!( :first_name => "Moe" , :last_name => "Sidgelack" , :email => "ms@email.com" , :password => "password" ),
+        User.create!( :first_name => "Barney" , :last_name => "Gumble" , :email => "bg@email.com" , :password => "password" ),
+        ])
+      end
+
+      it "displays both admins" do
+        render
+
+        expect(rendered).to match /Moe/
+        expect(rendered).to match /Barney/
+      end
+    end
+  end
+  
---
+++ spec/views/superadmins/index.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "superadmins/index" do
+
+  context "with 2 superadmins" do
+    before(:each) do
+      assign(:superadmins, [
+        User.create!( :first_name => "Moe" , :last_name => "Sidgelack" , :email => "ms@email.com" , :password => "password" ),
+        User.create!( :first_name => "Barney" , :last_name => "Gumble" , :email => "bg@email.com" , :password => "password" ),
+        ])
+      end
+
+      it "displays both superadmins" do
+        render
+
+        expect(rendered).to match /Moe/
+        expect(rendered).to match /Barney/
+      end
+    end
+  end

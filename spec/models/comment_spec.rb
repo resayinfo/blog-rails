a629e708c6a83a1665831681a00ff72c50b318e7
anthony maseda <ajmaseda@sixoverground.com>
2014-11-29 23:06:27 -0500

    model specs set up

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,15 @@
+# require 'spec_helper'
+#
+# describe Comment do
+#
+#   it "has a valid factory" do
+#     expect(FactoryGirl.create(:comment)).to be_valid
+#   end
+#   it "is invalid without a commenter" do
+#     expect(FactoryGirl.build(:comment, commenter: nil)).to_not be_valid
+#   end
+#   it "is invalid without a comment" do
+#     expect(FactoryGirl.build(:comment, body: nil)).to_not be_valid
+#   end
+#
+# end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+# require 'spec_helper'
+#
+# describe User do
+#   it "has a valid factory" do
+#     expect(FactoryGirl.create(:user)).to be_valid
+#   end
+#   it "is invalid without a first_name" do
+#     expect(FactoryGirl.build(:user, first_name: nil)).to be_invalid
+#   end
+#   it "is invalid without a last_name" do
+#     expect(FactoryGirl.build(:user, last_name: nil)).to be_invalid
+#   end
+#   it "is invalid without an email" do
+#     expect(FactoryGirl.build(:user, email: nil)).to be_invalid
+#   end
+#   it "is invalid without a password" do
+#     expect(FactoryGirl.build(:user, password: nil)).to be_invalid
+#   end
+# end

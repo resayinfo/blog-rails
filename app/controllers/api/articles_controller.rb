164e0dc0f8b017a37af79acb566a4d4c10249fa9
anthony maseda <ajmaseda@sixoverground.com>
2015-02-22 00:20:51 -0500

    show comments in postman

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -51,12 +51,12 @@ class Api::ArticlesController < Api::ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_article
     @article = Article.unscoped.find(params[:id])
-    render json: @article
+    render json: @article.as_json(include: [:comments])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def article_params
-    params.permit(:title, :author)
+    params.permit(:title, :author, :text)
   end
 
 end

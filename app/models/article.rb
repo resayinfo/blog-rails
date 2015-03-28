a70b02e961fdd4a33f55375f4f3f2e79bfbed05c
anthony maseda <ajmaseda@sixoverground.com>
2015-03-28 02:25:21 -0400

    min and max lengths

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ class Article < ActiveRecord::Base
   # validates :author, presence: true,
   #                   length: { minimum: 2, maximum: 25 }
   validates :title, presence: true,
-                    length: { minimum: 2, maximum: 25 }
+                    length: { minimum: 1, maximum: 25 }
   validates :text, presence: true,
-                    length: { minimum: 2, maximum: 1000000 }
+                    length: { minimum: 1, maximum: 10000000 }
 end
